64eb3a9c5225550a7977d9bf425efbf852a27242
jcdlls <julk470@gmail.com>
2026-03-29 03:58:45 -0600

    Actualizar main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
-import './styles.css'
+import { routeTree } from './routeTree.gen' // Generado automáticamente
+import './index.css'
 
 const router = createRouter({ routeTree })
 
@@ -12,8 +12,8 @@ declare module '@tanstack/react-router' {
   }
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </StrictMode>
 )
