cf7869ff8bd0df24c9dbd58d1c24f2c660e6ea51
jcdlls <julk470@gmail.com>
2026-03-29 03:33:04 -0600

    Actualizar Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,15 @@
 interface HeaderProps {
-    title: string
-    subtitle?: string
+  title: string
+  subtitle?: string
 }
 
 export function Header({ title, subtitle }: HeaderProps) {
-    return (
-        <div className="text-center">
-            <h1 className="text-4xl font-bold">
-                {title}
-                {subtitle && ` & ${subtitle}`}
-            </h1>
-        </div>
-    )
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold">
+        {title}
+        {subtitle && ` & ${subtitle}`}
+      </h1>
+    </div>
+  )
 }
