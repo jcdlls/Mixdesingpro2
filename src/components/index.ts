6dd94e21f6766425f6e63e42ba373cb35d5cfc6b
jcdlls <julk470@gmail.com>
2026-03-29 03:52:38 -0600

    Actualizar index.ts (barrel export)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as Button } from './Button'
-export { default as Card } from './Card'
-export { default as Header } from './Header'
+export { Button } from './Button'
+export { Card } from './Card'
+export { Header } from './Header'
